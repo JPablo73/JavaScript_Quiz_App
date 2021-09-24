a33de6c4d8f2bed9a266b76d755eaae2473cc64a
JPablo73 <82916926+JPablo73@users.noreply.github.com>
2021-09-24 14:39:51 -0700

    added question list

---
+++ questions.js
@@ -0,0 +1,43 @@
+const questions = [
+  {
+    question: "How do you write an IF statement in JavaScript?",
+    choice1: "if i = 5 then",
+    choice2: "if i = 5",
+    choice3: "if i == 5 then",
+    choice4: "if(i === 5)",
+    answer: choice4,
+  },
+  {
+    question:
+      "Which built-in method returns the character at the specified index?",
+    choice1: "characterAt()",
+    choice2: "getCharAt()",
+    choice3: "charAt()",
+    choice4: "None of the above",
+    answer: choice3,
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<scripting>",
+    choice2: "<script>",
+    choice3: "<js>",
+    choice4: "<javascipt>",
+    answer: choice2,
+  },
+  {
+    question: "How do you create a function in JavaScript",
+    choice1: "func = myFunction()",
+    choice2: "function: myFunc{}",
+    choice3: "let func = myfunc{}",
+    choice4: "function myFunc() {...}",
+    answer: choice4,
+  },
+  {
+    question: "How do you call a function?",
+    choice1: "call function myfunction()",
+    choice2: "call myFunction()",
+    choice3: "myFunction()",
+    choice4: "console.log(myFunction)",
+    answer: choice3,
+  },
+];
